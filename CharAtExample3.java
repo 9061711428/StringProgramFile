1e3a3b24c2bdd766051a16e640eb3373277e09e4
s@1996 <54677210+9061711428@users.noreply.github.com>
2020-01-03 16:24:03 +0530

    CharAtExample3

---
+++ CharAtExample3.java
@@ -0,0 +1,12 @@
+package stringmethods;
+
+public class CharAtExample3 {
+	public static void main(String args[])
+	{
+	String str="welcome to FACE portal";
+	int strLength=str.length();
+	System.out.println("character is 1st index is :"+str.charAt(1));//Fetching the 1st character
+	System.out.println("character at last index is:"+str.charAt(strLength-1));//fetching the last character of  the string.
+	}
+
+}
