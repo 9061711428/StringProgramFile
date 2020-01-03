68197023b5a1df4c225e53b571159bcd022fa200
s@1996 <54677210+9061711428@users.noreply.github.com>
2020-01-03 16:30:05 +0530

    ContainExample1

---
+++ ContainExample1.java
@@ -0,0 +1,12 @@
+package stringmethods;
+
+public class ContainExample1 {
+	public static void main(String args[])
+	{
+		String name="what do you know about me";
+		System.out.println(name.contains("do you know"));//search the particular word contain in a string
+		System.out.println(name.contains("about"));
+		System.out.println(name.contains("hello"));
+	}
+
+}
