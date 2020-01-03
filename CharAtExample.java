9442b37c32de6d6d4337f85d7b0f5c80450d04c8
s@1996 <54677210+9061711428@users.noreply.github.com>
2020-01-03 16:22:14 +0530

    CharAt Example

---
+++ CharAtExample.java
@@ -0,0 +1,11 @@
+package stringmethods;
+
+public class CharAtExample {
+	public static void main(String args[])
+	{
+		String name="FACE@2.0";
+		char ch=name.charAt(4);//returns the char value at 4th index/ index start from 0 to length-1.
+		System.out.println(ch);//print character
+	}
+
+}
