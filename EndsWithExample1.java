91302e7ca6bf6df7d4fffcb698c0f5ac7256d1a8
s@1996 <54677210+9061711428@users.noreply.github.com>
2020-01-03 16:31:59 +0530

    EndWithmethod Example1

---
+++ EndsWithExample1.java
@@ -0,0 +1,12 @@
+package stringmethods;//Example of endsWith()
+
+public class EndsWithExample1 {
+	public static void main(String args[])
+	{
+		String s1="java by FACE";
+		System.out.println(s1.endsWith("E"));//check whether string is end with "E",then return true
+		System.out.println(s1.endsWith("FACE"));//check whether string is end with "FACE"
+		
+	}
+
+}
